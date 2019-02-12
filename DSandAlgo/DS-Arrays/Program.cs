391f88c6d1ac01b8f01e0fbee148b0b693385b1e
asRandolph, Reginald (US) <reginald.randolph@lmco.com>
2019-02-12 15:58:30 -0500

    Starting Data Structures: Arrays

---
+++ DSandAlgo/DS-Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = { "a", "b", "c", "d" };
+            //4*4 = 16 bytes of storage
+
+            List<string> list = new List<string> { "a", "b", "c", "d" };
+            list.Insert(0, "e");
+            Console.WriteLine(list[0]);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            
+        }
+
+
+    }
+}
