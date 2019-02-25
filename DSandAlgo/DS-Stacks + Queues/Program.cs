9af906876f5bc905fc8a6996949ea8fa259dcac6
asRandolph, Reginald (US) <reginald.randolph@lmco.com>
2019-02-25 08:37:10 -0500

    Stack and Queues cont. added Array Stack

--- DSandAlgo/DS-Stacks + Queues/Program.cs
+++ DSandAlgo/DS-Stacks + Queues/Program.cs
@@ -37,6 +37,21 @@ namespace DS_Stacks___Queues
             Console.WriteLine("The current Top value is {0}!", stack.Peek());
             stack.Display();
             stack.DisplayCount();
+
+            //StackArray stack = new StackArray();
+            //stack.Push(2);
+            //stack.Push(4);
+            //stack.Push(6);
+            //stack.Push(8);
+            //stack.Push(10);
+            //stack.Display();
+            //stack.DisplayCount();
+            //Console.WriteLine("Popping {0} off the top!", stack.Pop());
+            //Console.WriteLine("Popping {0} off the top!", stack.Pop());
+            //Console.WriteLine("Popping {0} off the top!", stack.Pop());
+            //Console.WriteLine("The current Top value is {0}!", stack.Peek());
+            //stack.Display();
+            //stack.DisplayCount();
         }
     }
 }
---
+++ DSandAlgo/DS-Stacks + Queues/StackArray.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Stacks___Queues
+{
+    public class StackArray
+    {
+        List<int> array;
+        public StackArray()
+        {
+            array = new List<int>();
+        }
+
+        //O(n) if size is increased otherwise O(1)
+        public void Push(int value)
+        {
+            array.Add(value);
+        }
+
+        //O(1)
+        public int Pop()
+        {
+            int value = array.LastOrDefault();
+            array.Remove(array.Last());
+            return value;
+        }
+        //O(1)
+        public int Peek()
+        {
+            return array.LastOrDefault();
+        }
+        //O(n)
+        public void Display()
+        {
+            Console.WriteLine("The collection consist of:");
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        //O(1)
+        public void DisplayCount()
+        {
+            Console.WriteLine("The number of items in the Stack is: " + array.Count);
+        }
+    }
+}
