584904468e7c1bfd5e4c2b3eea7d084c8619f404
Reginald Randolph <rrandol3@fau.edu>
2019-03-01 18:49:14 -0500

    DS-Graphs finished

---
+++ DSandAlgo/DS-Graphs/Graph.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Graphs
+{
+    public class Graph
+    {
+        private int numberOfNodes = 0;
+        private Dictionary<int, List<int>> adjacentList = new Dictionary<int, List<int>>();
+        public void AddVertex(int node)
+        {
+            adjacentList.Add(node, new List<int>());
+            numberOfNodes++;
+        }
+
+        public void AddEdge(int node1, int node2)
+        {
+            //Unidirected Graph
+            adjacentList[node1].Add(node2);
+            adjacentList[node2].Add(node1);
+        }
+
+        public void ShowConnections()
+        {
+            var allNodes = adjacentList.Keys;
+            foreach (var node  in allNodes)
+            {
+                List<int> nodeConnections = adjacentList[node];
+                string connections = "";
+                
+                foreach (var vertex in nodeConnections)
+                {
+                    connections += vertex + " ";
+                }
+                Console.WriteLine(node + "-->" + connections);
+            }
+        }
+    }
+}
---
+++ DSandAlgo/DS-Graphs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Graphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph();
+            for (int i = 0; i < 7; i++)
+            {
+                graph.AddVertex(i);
+            }
+            graph.AddEdge(3, 1);
+            graph.AddEdge(3, 4);
+            graph.AddEdge(4, 2);
+            graph.AddEdge(4, 5);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(1, 0);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(6, 5);
+            graph.ShowConnections();
+        }
+    }
+}
