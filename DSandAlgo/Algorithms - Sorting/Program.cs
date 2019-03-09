a6016b390f767c6d3bbe4bf4b75cf715b0ec992e
Reginald Randolph <rrandol3@fau.edu>
2019-03-09 14:27:14 -0500

    Finished Sorting

--- DSandAlgo/Algorithms - Sorting/Program.cs
+++ DSandAlgo/Algorithms - Sorting/Program.cs
@@ -10,100 +10,130 @@ namespace Algorithms___Sorting
     {
         static void Main(string[] args)
         {
-            //int[] intArray = { 8, 2, 4, 33, 11, 5, 6, 7 };
-            //string[] strArray = { "a", "z", "d", "r", "c", "u" };
-
-            //Console.WriteLine("Before intArray sort:");
-            //foreach (var item in intArray)
-            //{
-            //    Console.Write(" " + item);
-            //}
-            //Console.WriteLine();
-            //Array.Sort(intArray);
-            //Console.WriteLine("After intArray sort:");
-            //foreach (var item in intArray)
-            //{
-            //    Console.Write(" " + item);
-            //}
-            //Console.WriteLine();
-            //Console.WriteLine("Before strArray sort:");
-            //for (int i = 0; i < strArray.Length; i++)
-            //{
-            //    Console.Write(" " + strArray[i]);
-            //}
-            //Console.WriteLine();
-            //Console.WriteLine("Before strArray sort:");
-            //Array.Sort(strArray);
-            //for (int i = 0; i < strArray.Length; i++)
-            //{
-            //    Console.Write(" " + strArray[i]);
-            //}
-            //Console.WriteLine();
             int[] numArray = { 9, 5, 3, 6, 8, 2, 1 };
-            Console.WriteLine("numArray before sort:");
+            SelectionSort(numArray);
             for (int i = 0; i < numArray.Length; i++)
             {
                 Console.Write(" " + numArray[i]);
             }
-            Console.WriteLine();
-            BubbleSortVisualizer(numArray);
-            Console.WriteLine();
-            Console.WriteLine("numArray after sort:");
-            BubbleSort(numArray);
-            for (int i = 0; i < numArray.Length; i++)
-            {
-                Console.Write(" " + numArray[i]);
-            }
-
             Console.WriteLine();
         }
 
         //O(n^2)
-        public static void BubbleSort(int[] intArray)
+        public static void BubbleSort(int[] numArray)
         {
             //{ 9, 5, 3, 6, 8, 2, 1 };
-            int n = intArray.Length;
-            for (int i = 0; i < n; i++)
+            int n = numArray.Length;//number of items in array
+            for (int i = 0; i < n; i++)//keeps track of the traversal count through the array
             {
-                for (int j = 0; j < n - 1; j++)
+                for (int j = 0; j < n - 1; j++)//used for comparision of items in the array
                 {
-                    if (intArray[j] > intArray[j + 1])
+                    if (numArray[j] > numArray[j + 1])//check to see if j > j + 1, if true, then swap
                     {
-                        int temp = intArray[j];
-                        intArray[j] = intArray[j + 1];
-                        intArray[j + 1] = temp;
+                        int temp = numArray[j];
+                        numArray[j] = numArray[j + 1];
+                        numArray[j + 1] = temp;
                     }
                 }
             }
         }
-        public static void BubbleSortVisualizer(int[] intArray)
+
+        //O(n^2)
+        public static void SelectionSort(int[] numArray)
         {
-            Console.WriteLine("assign n to " + intArray.Length);
-            int n = intArray.Length;
-            Console.WriteLine("Enter outer loop i");
-            for (int i = 0; i < n; i++)
+            //{ 9, 5, 3, 6, 8, 2, 1 };
+            int n = numArray.Length;//number of items in array
+            for (int i = 0; i < n; i++)//tracks the items already in their right position
             {
-                Console.WriteLine("Enter inner loop j");
-                for (int j = 0; j < n - 1; j++)
+                int indexOfSmallest = i;//index of current smallest
+                for (int j = i + 1; j < n; j++)//compares each item to find the smallest index, (j = i + 1 bc no need to check items that already in right position)
                 {
-                    Console.WriteLine("Is intArray[{0}]({1}) greater than intArray[{2}]({3})?", j, intArray[j], j + 1, intArray[j + 1]);
-                    if (intArray[j] > intArray[j + 1])
+                    if (numArray[indexOfSmallest] > numArray[j])//if new smallest is > than j 
                     {
-                        Console.WriteLine("Yes swap intArray[{0}]({1}) with intArray[{2}]({3})", j, intArray[j], j + 1, intArray[j + 1]);
-                        int temp = intArray[j];
-                        intArray[j] = intArray[j + 1];
-                        intArray[j + 1] = temp;
+                        indexOfSmallest = j;//new smallest = j
                     }
-                    Console.WriteLine("Increment j = {0}", j + 1);
                 }
-                Console.WriteLine("Increment i = {0}", i + 1);
-                Console.WriteLine("{0} pass thru:", i + 1);
-                for (int k = 0; k < n; k++)
-                {
-                    Console.Write(" " + intArray[k]);
-                }
-                Console.WriteLine();
+                //swap new smallest(indexOfSmallest) with previous smallest(i)
+                int temp = numArray[i];
+                numArray[i] = numArray[indexOfSmallest];
+                numArray[indexOfSmallest] = temp;
             }
         }
+
+        //O(n^2)
+        public static void InsertionSort(int[] numArray)
+        {
+            //{ 9, 5, 3, 6, 8, 2, 1 };
+            int n = numArray.Length;//number of items in array
+            int temp;
+            int j;
+            for (int i = 1; i < n; i++)//used to pick value we want to insert in sorted left array
+            {
+                temp = numArray[i];//the value we want to insert
+                for (j = i; j > 0 && (numArray[j - 1] > temp); j--)//user for the comparision, values are shifted to the right until we find the appropriate position of the temp value
+                {
+                    numArray[j] = numArray[j - 1];
+                }
+                numArray[j] = temp;
+            }
+        }
+
+        //O (n log n) TODO: need to do some more research and finish
+        public static int[] MergeSort(int[] numArray)
+        {
+            //{ 9, 5, 3, 6, 8, 2, 1 };
+            if (numArray.Length == 1)
+            {
+                return numArray;
+            }
+
+            int low = 0;
+            int high = numArray.Length;
+            int mid = low + (high - low) / 2;
+            int[] left = new int[mid];
+            int[] right = new int[numArray.Length - left.Length];
+            for (int i = 0; i < mid; i++)
+            {
+                left[i] = numArray[i];
+            }
+            int k = mid;
+            for (int j = 0; k < high; j++, k++)
+            {
+                right[j] = numArray[k];
+            }
+
+            return Merge(MergeSort(left),MergeSort(right));
+        }
+
+        public static int[] Merge(int[] left, int[] right)
+        {
+            int[] newArray = new int[left.Length + right.Length];
+            int i = 0;
+            int leftIndex = 0;
+            int rightIndex = 0;
+            while (leftIndex < left.Length && rightIndex < right.Length)
+            {
+                if (left[leftIndex] < right[rightIndex])
+                {
+                    newArray[i] = left[leftIndex];
+                    leftIndex++;
+                    i++;
+                }
+                else
+                {
+                    newArray[i] = right[rightIndex];
+                    rightIndex++;
+                    i++;
+                }
+            }
+            return newArray;
+        }
+
+        //O(n^2) TODO: need to research and implement
+        public static void QuickSort(int[] numArray)
+        {
+
+        }
+        
     }
 }
